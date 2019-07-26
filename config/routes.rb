378cd12d0f62a23877c3983da0e4f4ad2d3dd965
Victor Hermes <me.victorhermes@gmail.com>
2019-07-25 21:01:49 -0300

    delete okay

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
       resources :contacts
     end
   end
+ 
+  # constraints subdomain: 'api' do
+  #   scope module: 'api' do
+  #     namespace :v1 do
+  #       resources :contacts
+  #     end
+  #   end
+  # end
 end
